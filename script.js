0a0f60176349cf3d681149a6329c11983e04f2ba
Pawel <pawel.werbowy@gmail.com>
2024-01-17 20:06:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const searchInput = document.querySelector("[data-search]");
 let users = [];
 searchInput.addEventListener("input", (e) => {
   const value = e.target.value.toLowerCase();
-  console.log(value);
+  console.log(users);
 });
 
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((data) => {
-    user = data.map((user) => {
+    users = data.map((user) => {
       const card = userCardTemplate.content.cloneNode(true).children[0];
       const header = card.querySelector("[data-header]");
       const body = card.querySelector("[data-body]");
       header.textContent = user.name;
       body.textContent = user.email;
       userCardContainer.append(card);
-      return { name: user.name, email: user.email };
+      return { name: user.name, email: user.email, element: card };
     });
   });
