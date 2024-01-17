5b9610cf786b3e6f919e07a6139ee6a4f20d3708
Pawel <pawel.werbowy@gmail.com>
2024-01-17 20:01:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const userCardTemplate = document.querySelector("[data-user-template]");
 const userCardContainer = document.querySelector("[data-user-cards-container]");
+const searchInput = document.querySelector("[data-search]");
 
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
