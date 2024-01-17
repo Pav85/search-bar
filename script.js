d6e165893075b5f7334f35cedf0e5aaf7ed7fa16
Pawel <pawel.werbowy@gmail.com>
2024-01-17 19:53:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const userCardTemplate = document.querySelector("[data-user-template]");
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((data) => {
-    const card = userCardTemplate.content.cloneNode(true).children[0];
-    console.log(card);
+    data.forEach((user) => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+      console.log(user);
+    });
   });
